629a9dd6d6813899c059fe2d55fea71f7c99b0a6
Fledrox <37022473+Fledrox@users.noreply.github.com>
2020-01-19 15:37:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//discord.js requirements / client constant
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+//token
+const token = "NTgyMTczMjMyMzk2ODk0MjE4.XiQ67w.qrToyrqiM30FU7ghH4TelSlV6Xo";
+
+
+//initialisation message
+client.on("ready", () =>{
+    console.log("Mario's QPUs are aligned, bot is up and ready");
+});
+
+//start of commands
+
+//test command
+btoa.on("message", msg=>{
+    if (msg.content === "First try"){
+        msg.reply("IDIOT");
+    }
+})
+
+
+
+//client+token link
+client.login(token);
