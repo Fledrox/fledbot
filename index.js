0c7dc117eac346ce797b6c094643d2a6e8cb1817
Fledrox <37022473+Fledrox@users.noreply.github.com>
2021-03-25 00:09:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,101 @@
-//discord.js requirements / client constant
-const Discord = require("discord.js");
+//Start of code
+
+//Requirements
+const fs = require('fs');
+const Discord = require('discord.js');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFolders = fs.readdirSync('./commands');
+const commandFiles = fs.readdirSync('./commands').filter(file =>file.endsWith('.js'));
 
-//token
-const token = "NTgyMTczMjMyMzk2ODk0MjE4.XiQ67w.qrToyrqiM30FU7ghH4TelSlV6Xo";
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+
+const config = require('./config.json');
 
 
-//initialisation message
+//Login
+client.login(config.token);
+
+//On client startup
 client.on("ready", () =>{
-    console.log("Mario's QPUs are aligned, bot is up and ready");
+    console.log("Bot is up and ready.");
+
+    //Discord presence
+    client.user.setPresence({
+        activity: {
+            name: 'Playing e621.net'
+        }, status: 'idle'
+    });
 });
 
-//start of commands
+//Listen for commands
 
-//test command
-client.on("message", msg=>{
-    if (msg.content === "First try"){
-        msg.reply("IDIOT");
+//Check if it has prefix, or if author is a bot
+client.on('message', message => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    //Splices arguments and sets them to lowercase
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+    //Include command aliases
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    //If it's not a command
+    if (!command) return;
+
+    //If it's not in #bot-testing or #bots
+    if (message.channel.id == 699639314501337088 || message.channel.id == 647788572627435520){
+        true;
+    } else {
+        message.channel.send("Wrong channel");
+        return console.log(message.channel.id);
     }
-})
 
+    //If command is done in DMs and guildOnly = true, ignores command
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply("<:fledpokerface:821087177525166151> I can't do that in DMs!");
+    }
 
+    //Check if author has required permissions for command
+    if (command.permissions) {
+        const authorPerms = message.channel.permissionsFor(message.author);
+        if (!authorPerms || !authorPerms.has(command.permissions)) {
+     	return message.channel.send(`<:fledbonk:758604306227396619> <@${message.author.id}> You can't do that lol`);
+        }
+    }
 
-//client+token link
-client.login(token);
+    //Checks if command must have arguments
+    if (command.args && !args.length) {
+        return message.channel.send(`<:fledpokerface:821087177525166151> This command needs arguments! Type \`/help <command>\` for a list of them.`);
+    }
+
+    //EXECUTE COMMAND
+	try {
+		command.execute(message, client, args);
+        console.log(message.author.username, "executed command", command.name);
+	} catch (error) {
+        console.log("SHOWING ERROR LOGS FOR COMMAND: ", commandName);
+		console.error(error);
+		message.channel.send('<:fledburger:762283936742506526> Executing that command was an epic fail');
+	}
+});
+
+//tests index.js
+client.on('message', message => {
+    if (message.content === config.prefix + 'main'){
+        try{
+            message.channel.send('<:fledtroll:821813805210271774> Index is up.')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
