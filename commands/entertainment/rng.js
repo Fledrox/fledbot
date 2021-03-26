717f801c6276aa8503bec9d53d39223c7fe1008a
Fledrox <37022473+Fledrox@users.noreply.github.com>
2021-03-26 18:03:46 +0100

    add NaN response
    if NaN, reply <args> is not a number

--- commands/entertainment/rng.js
+++ commands/entertainment/rng.js
@@ -11,6 +11,9 @@ module.exports = {
 	 * @param {string[]} args
 	 */
     execute: function (message, _client, args) {
+        if (isNaN(args[0])) {message.channel.send(args.join(' ') + " is not a number");
+    } else {
         message.channel.send(Math.round(Math.random() * (parseInt(args[0]))));
+    }
     },
 };
