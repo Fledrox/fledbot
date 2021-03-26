56a72d5c92d201611711cf820314a308856458e0
Fledrox <37022473+Fledrox@users.noreply.github.com>
2021-03-27 00:41:14 +0100

    Update reload.js
    it's better when people other than me don't use that command

--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -6,7 +6,6 @@ module.exports = {
     description: "Reloads a command",
     usage: "<args>",
     guildOnly: false,
-    permissions: ['SOME_PERMISSION','OTHER_PERMISSION'],
     args: true,
 
     //JSDocs
@@ -20,6 +19,10 @@ module.exports = {
     execute: function (message, client, args) {
 
         try {
+            //Ignores if not bot dev
+            if (message.author.id != 343354536615739402) {
+                return message.channel.send("You are not Fledrox");
+            }
             //commandName is the argument
             const commandName = args[0].toLowerCase();
             //Gets bot's commands and aliases
