0d56217669f2b3d073888fbe0ff976964b1a9f15
Fledrox <37022473+Fledrox@users.noreply.github.com>
2021-03-27 12:55:55 +0100

    move token to standalone file
    So I don't have to remove it every time I change config.json if I test the bot on VSCode. Also added comments on index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//Requirements
 const { Client, Collection } = require('discord.js');
-
 const client = new Client();
 
 client.commands = new Collection();
 client.config = require('./config.json');
+client.token = require('./token.json');
 
 require(`./handlers/commands`).handle(client);
 require(`./handlers/events`).handle(client);
 
-client.login(client.config.token);
+//Login
+client.login(client.token.token);
